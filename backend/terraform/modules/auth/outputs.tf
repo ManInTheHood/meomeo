6c5ebe4ec851ad76590b3d56b23c1a545a6959be
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2026-05-22 12:52:31 +0200

    chore(terraform): split dev and prod environments (#35)

--- backend/terraform/modules/auth/outputs.tf
+++ backend/terraform/modules/auth/outputs.tf
@@ -2,10 +2,42 @@ output "user_pool_id" {
   value = aws_cognito_user_pool.main.id
 }
 
+output "user_pool_arn" {
+  value = aws_cognito_user_pool.main.arn
+}
+
 output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.flutter_app.id
 }
 
 output "user_pool_endpoint" {
   value = aws_cognito_user_pool.main.endpoint
+}
+
+output "issuer" {
+  value = local.issuer
+}
+
+output "jwks_uri" {
+  value = "${local.issuer}/.well-known/jwks.json"
+}
+
+output "domain" {
+  value = local.hosted_ui_domain
+}
+
+output "domain_prefix" {
+  value = aws_cognito_user_pool_domain.main.domain
+}
+
+output "callback_urls" {
+  value = var.callback_urls
+}
+
+output "logout_urls" {
+  value = var.logout_urls
+}
+
+output "oauth_scopes" {
+  value = var.oauth_scopes
 }
